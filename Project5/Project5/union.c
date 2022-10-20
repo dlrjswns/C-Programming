b277f8eac2d33c2d39bbfd0c3084415f80d7dbcd
dlrjswns <leekyunjun@gmail.com>
2022-10-20 09:47:48 +0900

    2022-10-20 enum, bitField

---
+++ Project5/Project5/union.c
@@ -0,0 +1,18 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <string.h>
+//
+//union uchr {
+//	char chr1;
+//	char chr2[3];
+//} data;
+//
+//int main(void) {
+//	strcpy(data.chr2, "AB");
+//	printf("data.chr1 = %c\n", data.chr1);
+//	printf("data.chr2 = %c\n", data.chr2);
+//	data.chr1 = 'C';
+//	printf("data.chr1 = %c\n", data.chr1);
+//	printf("data.chr2 = %s\n", data.chr2);
+//	return 0;
+//}
---
+++ Project5/Project5/union1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+enum week {sun, mon, tue, wed, thu, fri, sat};
+
+int main(void) {
+	enum week day1;
+	day1 = fri;
+	printf();
+	day1 = 100;
+	printf("%d", day1);
+	return 0;
+}
